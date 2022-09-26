1de6f753aa118429359930a5e8c297025ef7e557
portuna85 <portuna85@gmail.com>
2022-09-26 13:06:10 +0900

    Validation Update

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -182,7 +182,7 @@ public class ValidationItemControllerV2 {
 
         //검증에 실패하면 다시 입력 폼으로
         if (bindingResult.hasErrors()) {
-            log.info("errors={} ", bindingResult);
+            log.info("errors = {} ", bindingResult);
             return "validation/v2/addForm";
         }
 
