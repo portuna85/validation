340c126480d13a0345dcf12aa0440a5846968b5f
portuna85 <portuna85@gmail.com>
2022-09-26 16:20:54 +0900

    Validation Update

--- src/main/java/hello/itemservice/web/ItemValidator.java
+++ src/main/java/hello/itemservice/web/ItemValidator.java
@@ -21,7 +21,6 @@ public class ItemValidator implements Validator {
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "itemName", "required");
 
 
-
         //검증 로직
         if (!StringUtils.hasText(item.getItemName())) {
             errors.rejectValue("itemName", "required");
@@ -33,6 +32,14 @@ public class ItemValidator implements Validator {
 
         if (item.getQuantity() == null || item.getQuantity() >= 9999) {
             errors.rejectValue("quantity", "max", new Object[]{9999}, null);
+        }
+
+        //특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                errors.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
         }
     }
 }
